09a4fa1ef2a659569a4adf14e66223330c76ce35
薛黄正 <1928828552@qq.com>
2023-05-22 17:43:58 +0800

    feat: wkt转大写

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ type GeojsonType<T = {}, K = {}> = {
 // wkt转geojson
 function wktToGeometry(wkt: string) {
   try {
-    const type = wkt.match(/^[A-Z]+(?=\()/)![0].toUpperCase()
+    const type = wkt.match(/^[A-Z]+(?=\()/)![0]
     let geometry: GeometryType | undefined
 
     if (type === 'POINT') {
@@ -107,7 +107,8 @@ export function wktToGeojson(wkt: string) {
       .replaceAll('( ', '(')
       .replaceAll(' )', ')')
       .replaceAll(') ', ')')
-    const type = wkt.match(/^[A-Z]+(?=\()/)![0].toUpperCase()
+      .toUpperCase()
+    const type = wkt.match(/^[A-Z]+(?=\()/)![0]
     if (type === 'GEOMETRYCOLLECTION') {
       wkt = wkt.substring(19, wkt.length - 1)
       const resArr = wkt.split(/,(?=[A-Z]+)/)
