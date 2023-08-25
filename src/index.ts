ffcc6d5cf111c2dea86046dc0b974a8c8c508279
薛黄正 <1928828552@qq.com>
2023-08-25 10:55:24 +0800

    feat: geojson转wkt兼容geometrycollection类型

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,23 @@
-/**geojson类型定义 */
-type GeometryType = {
-  type: string
-  coordinates: number[] | number[][] | number[][][] | number[][][][]
-}
-type FeatureType<T = {}> = {
-  type: string
-  id?: string | number
-  bbox?: number[]
-  geometry?: GeometryType
-  properties?: T
-}
-type GeojsonType<T = {}, K = {}> = {
-  type: string
-  crs?: {
-    type: string
-    properties: K
-  }
-  properties?: T
-  bbox?: number[]
-  coordinates?: number[] | number[][] | number[][][] | number[][][][]
-  geometry?: GeometryType
-  geometries?: GeometryType[]
-  features?: FeatureType<T>[]
-  gid?: number
-}
-
+import {
+  Feature,
+  Geometry,
+  Point,
+  MultiPoint,
+  LineString,
+  MultiLineString,
+  Polygon,
+  MultiPolygon,
+  GeoJSON,
+  GeometryCollection,
+  FeatureCollection,
+} from 'geojson'
+type GeometryExcludeCollection =
+  | Point
+  | MultiPoint
+  | LineString
+  | MultiLineString
+  | Polygon
+  | MultiPolygon
 // wkt转geojson
 function wktToGeometry(wkt: string) {
   try {
@@ -34,7 +27,7 @@ function wktToGeometry(wkt: string) {
       return
     }
     const type = typeStr[0]
-    let geometry: GeometryType | undefined
+    let geometry: Geometry | undefined
 
     if (type === 'POINT') {
       const res = wkt.match(/(-*\d+\.*\d*\s-*\d+\.*\d*)/g)
@@ -126,9 +119,11 @@ function wktToGeometry(wkt: string) {
   }
 }
 export function wktToGeojson(wkt: string) {
+  console.log(wkt)
+
   try {
     wkt = wkt
-      .replaceAll('\n', '')
+      .replaceAll('\n', ' ')
       .replaceAll(/\s+/g, ' ')
       .replaceAll(' ,', ',')
       .replaceAll(', ', ',')
@@ -146,21 +141,21 @@ export function wktToGeojson(wkt: string) {
     if (type === 'GEOMETRYCOLLECTION') {
       wkt = wkt.substring(19, wkt.length - 1)
       const resArr = wkt.split(/,(?=[A-Z]+)/)
-      const features: FeatureType[] = []
+      const features: Feature[] = []
       for (let i = 0; i < resArr.length; i++) {
         const geometry = wktToGeometry(resArr[i])
         if (!geometry) return
-        features.push({ type: 'Feature', geometry })
+        features.push({ type: 'Feature', geometry, properties: {} })
       }
       return { type: 'FeatureCollection', features }
     } else {
       const resArr = wkt.split(/,(?=[A-Z]+)/)
       if (resArr.length > 1) {
-        const features: FeatureType[] = []
+        const features: Feature[] = []
         for (let i = 0; i < resArr.length; i++) {
           const geometry = wktToGeometry(resArr[i])
           if (!geometry) return
-          features.push({ type: 'Feature', geometry })
+          features.push({ type: 'Feature', geometry, properties: {} })
         }
         return { type: 'FeatureCollection', features }
       } else {
@@ -189,76 +184,152 @@ export function wktToGeojson(wkt: string) {
 }
 
 // geojson转wkt
-function geometryToWkt(geometry: GeometryType) {
+function geometryToWkt(geometry: GeometryExcludeCollection) {
+  if (!geometry) {
+    console.log('没有geojson数据')
+    return ''
+  }
   const { type } = geometry
   if (!type) {
-    console.log('这个geometry没有type')
+    console.log('数据中没有type')
     return ''
   }
   if (typeof type !== 'string') {
-    console.log('这个geometry的type不是字符串')
+    console.log('数据中的type不是字符串')
     return ''
   }
   const coords = geometry.coordinates
   if (!coords) {
-    console.log('这个geometry没有coordinates')
+    console.log('数据中没有coordinates')
     return ''
   }
   if (!(coords instanceof Array)) {
-    console.log('这个geometry的coordinates不是数组')
+    console.log('数据的coordinates不是数组')
     return ''
   }
   if (coords.length === 0) {
-    console.log('这个geometry的coordinates是空的')
+    console.log('数据的coordinates是空的')
     return ''
   }
   const wktCoords: string[] = []
 
   switch (type.toUpperCase()) {
     case 'POINT':
+      if (coords.length < 2) {
+        console.log('type为Point的coordinates格式不对-->', coords)
+        return ''
+      }
+      for (let i = 0; i < coords.length; i++) {
+        if (typeof coords[i] !== 'number' || isNaN(coords[i] as number)) {
+          console.log('type为Point的coordinates格式不对-->', coords[i])
+          return ''
+        }
+      }
       wktCoords.push(coords.slice(0, 2).join(' '))
       break
     case 'LINESTRING':
     case 'MULTIPOINT':
-      coords.forEach((item: any) => {
-        item instanceof Array && wktCoords.push(item.slice(0, 2).join(' '))
-      })
+      for (let i = 0; i < coords.length; i++) {
+        const coord = coords[i] as number[]
+        if (!(coord instanceof Array) || coord.length < 2) {
+          console.log(
+            'type为MultiPoint或LineString的coordinates格式不对-->',
+            coord
+          )
+          return ''
+        }
+        for (let j = 0; j < coord.length; j++) {
+          if (typeof coord[j] !== 'number' || isNaN(coord[j] as number)) {
+            console.log(
+              'type为MultiPoint或LineString的coordinates格式不对-->',
+              coord[j]
+            )
+            return ''
+          }
+        }
+        wktCoords.push(coord.slice(0, 2).join(' '))
+      }
       break
     case 'POLYGON':
     case 'MULTILINESTRING':
-      coords.forEach((item: any) => {
+      for (let i = 0; i < coords.length; i++) {
+        const item = coords[i]
         const ringCoords: string[] = []
-        item instanceof Array &&
-          item.forEach((ele: any) => {
-            ele instanceof Array && ringCoords.push(ele.slice(0, 2).join(' '))
-          })
+        if (!(item instanceof Array)) {
+          console.log(
+            'type为Polygon或MultiLineString的coordinates格式不对-->',
+            item
+          )
+          return ''
+        }
+        for (let j = 0; j < item.length; j++) {
+          const ele = item[j]
+          if (!(ele instanceof Array) || ele.length < 2) {
+            console.log(
+              'type为Polygon或MultiLineString的coordinates格式不对-->',
+              ele
+            )
+            return ''
+          }
+          for (let k = 0; k < ele.length; k++) {
+            if (typeof ele[k] !== 'number' || isNaN(ele[k] as number)) {
+              console.log(
+                'type为Polygon或MultiLineString的coordinates格式不对-->',
+                ele[k]
+              )
+              return ''
+            }
+          }
+          ringCoords.push(ele.slice(0, 2).join(' '))
+        }
         wktCoords.push(`(${ringCoords.join(',')})`)
-      })
+      }
       break
     case 'MULTIPOLYGON':
-      coords.forEach((item: any) => {
+      for (let i = 0; i < coords.length; i++) {
+        const item = coords[i]
         const polyCoords: string[] = []
-        item instanceof Array &&
-          item.forEach((ele: any) => {
-            const ringCoords: string[] = []
-            ele instanceof Array &&
-              ele.forEach((mem: any) => {
-                mem instanceof Array &&
-                  ringCoords.push(mem.slice(0, 2).join(' '))
-              })
-            polyCoords.push(`(${ringCoords.join(',')})`)
-          })
+        if (!(item instanceof Array)) {
+          console.log('type为MultiPolygon的coordinates格式不对-->', item)
+          return ''
+        }
+        for (let j = 0; j < item.length; j++) {
+          const ele = item[j]
+          const ringCoords: string[] = []
+          if (!(ele instanceof Array)) {
+            console.log('type为MultiPolygon的coordinates格式不对-->', ele)
+            return ''
+          }
+          for (let k = 0; k < ele.length; k++) {
+            const mem = ele[k]
+            if (!(mem instanceof Array) || mem.length < 2) {
+              console.log('type为MultiPolygon的coordinates格式不对-->', mem)
+              return ''
+            }
+            for (let n = 0; n < mem.length; n++) {
+              if (typeof mem[n] !== 'number' || isNaN(mem[n] as number)) {
+                console.log(
+                  'type为Polygon或MultiLineString的coordinates格式不对-->',
+                  mem[n]
+                )
+                return ''
+              }
+            }
+            ringCoords.push(mem.slice(0, 2).join(' '))
+          }
+          polyCoords.push(`(${ringCoords.join(',')})`)
+        }
         wktCoords.push(`(${polyCoords.join(',')})`)
-      })
+      }
       break
     default:
-      console.log('这个geojson的type值好像不太对-->', type)
+      console.log(`这个geojson中的type值好像不太对--> type: ${type}`)
       return ''
   }
   return `${type.toUpperCase()}(${wktCoords.join(',')})`
 }
 
-export function geojsonToWkt(geojson: GeojsonType) {
+export function geojsonToWkt(geojson: GeoJSON) {
   if (!geojson) {
     console.log('没有geojson数据')
     return ''
@@ -282,24 +353,78 @@ export function geojsonToWkt(geojson: GeojsonType) {
       'MULTIPOLYGON',
     ].includes(type.toUpperCase())
   ) {
-    return geometryToWkt(geojson as GeometryType)
+    return geometryToWkt(geojson as GeometryExcludeCollection)
   } else if (type.toUpperCase() === 'FEATURE') {
-    return geometryToWkt(geojson.geometry as GeometryType)
+    const { geometry } = geojson as Feature
+    if (!geometry) {
+      console.log('type为Feature的数据没有geometry-->', geojson)
+      return ''
+    }
+    if (geometry.type.toUpperCase() === 'GEOMETRYCOLLECTION') {
+      const { geometries } = geometry as GeometryCollection
+      return `GEOMETRYCOLLECTION(${handleGeometries(geometries)})`
+    }
+    return geometryToWkt(geometry as GeometryExcludeCollection)
   } else if (type.toUpperCase() === 'GEOMETRYCOLLECTION') {
-    const wkts: string[] = []
-    geojson.geometries?.forEach((item) => {
-      wkts.push(geometryToWkt(item))
-    })
-    return `GEOMETRYCOLLECTION(${wkts.join(',')})`
+    const { geometries } = geojson as GeometryCollection
+    const result = handleGeometries(geometries)
+    return `GEOMETRYCOLLECTION(${result})`
   } else if (type.toUpperCase() === 'FEATURECOLLECTION') {
     const wkts: string[] = []
-    geojson.features?.forEach((item) => {
-      wkts.push(geometryToWkt(item.geometry as GeometryType))
-    })
+    const { features } = geojson as FeatureCollection
+    if (!features) {
+      console.log('type为FeatureCollection的数据没有features-->', geojson)
+      return ''
+    }
+    if (!(features instanceof Array)) {
+      console.log('type为FeatureCollection的数据中features不是数组-->', geojson)
+      return ''
+    }
+    for (let i = 0; i < features.length; i++) {
+      const item = features[i]
+      const { type } = item
+      if (!type) {
+        console.log('这个feature没有type-->', item)
+        return ''
+      }
+      if (typeof type !== 'string') {
+        console.log('这个feature的type不是字符串-->', item)
+        return ''
+      }
+      const { geometry } = item
+      if (!geometry) {
+        console.log('这个feature没有geometry-->', item)
+        return ''
+      }
+      if (geometry.type.toUpperCase() === 'GEOMETRYCOLLECTION') {
+        const { geometries } = geometry as GeometryCollection
+        wkts.push(handleGeometries(geometries))
+      } else {
+        wkts.push(geometryToWkt(geometry as GeometryExcludeCollection))
+      }
+    }
     return `GEOMETRYCOLLECTION(${wkts.join(',')})`
   } else {
-    console.log('这个geojson的type值好像不太对-->', type)
+    console.log(`这个geojson的type值好像不太对--> type: ${type}`)
     return ''
   }
 }
 
+function handleGeometries(geometries: any) {
+  const wkts: string[] = []
+  if (!geometries) {
+    console.log('type为GeometryCollection的数据没有geometries-->', geometries)
+    return ''
+  }
+  if (!(geometries instanceof Array)) {
+    console.log(
+      'type为GeometryCollection的数据中geometries不是数组-->',
+      geometries
+    )
+    return ''
+  }
+  for (let i = 0; i < geometries.length; i++) {
+    wkts.push(geometryToWkt(geometries[i]))
+  }
+  return wkts.join(',')
+}
