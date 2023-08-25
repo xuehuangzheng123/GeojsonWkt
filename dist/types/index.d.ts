d2967636883488bf265910084d6faa1c91355ff0
薛黄正 <1928828552@qq.com>
2023-08-25 10:56:19 +0800

    feat: 打包

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,37 +1,11 @@
-/**geojson类型定义 */
-type GeometryType = {
-    type: string;
-    coordinates: number[] | number[][] | number[][][] | number[][][][];
-};
-type FeatureType<T = {}> = {
-    type: string;
-    id?: string | number;
-    bbox?: number[];
-    geometry?: GeometryType;
-    properties?: T;
-};
-type GeojsonType<T = {}, K = {}> = {
-    type: string;
-    crs?: {
-        type: string;
-        properties: K;
-    };
-    properties?: T;
-    bbox?: number[];
-    coordinates?: number[] | number[][] | number[][][] | number[][][][];
-    geometry?: GeometryType;
-    geometries?: GeometryType[];
-    features?: FeatureType<T>[];
-    gid?: number;
-};
+import { Feature, Geometry, GeoJSON } from 'geojson';
 export declare function wktToGeojson(wkt: string): {
     type: string;
-    features: FeatureType<{}>[];
+    features: Feature<Geometry, import("geojson").GeoJsonProperties>[];
     geometry?: undefined;
 } | {
     type: string;
-    geometry: GeometryType;
+    geometry: Geometry;
     features?: undefined;
 } | undefined;
-export declare function geojsonToWkt(geojson: GeojsonType): string;
-export {};
+export declare function geojsonToWkt(geojson: GeoJSON): string;
